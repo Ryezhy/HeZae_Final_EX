99c5d08fb0d8d95165e137751701c8feda9e3825
MAXHSS <2650610676@qq.com>
2024-08-22 14:46:35 +0800

    1.0.3：完善了配置文件的远程和本地的获取方式，主页的内容添加了插槽

--- src/components/viewComponents/viewLiteComponents/settingsLiteViewComponent.vue
+++ src/components/viewComponents/viewLiteComponents/settingsLiteViewComponent.vue
@@ -11,56 +11,137 @@ interface BaseConfig {
   default: string | number;
   content: string[];
 }
-let systemConfig = ref<BaseConfig[]>([])
+let systemConfig = ref<BaseConfig[]>([])//默认保留的系统配置
+let mobileConfig = ref<BaseConfig[]>([])//移动端配置
+let systemDefaultConfig = ref<BaseConfig[]>([])
+let mobileDefaultConfig = ref<BaseConfig[]>([])
 const systemConfigIsLoading = ref(false)
-const init =async()=>{//初始化
-  //检查本地缓存
-  if (localStorage.getItem('systemConfig')) {
-    ElMessage.success('从本地缓存加载配置成功')
-
-    systemConfig.value = JSON.parse(localStorage.getItem('systemConfig') || '');
-  }else {
+const mobileConfigIsLoading = ref(false)
+const getDefaultConfig =async(type:string)=> {//获取默认参数
+  if (type == 'system') {
     systemConfigIsLoading.value = true;
-    await getDefaultSystemConfig();
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/static/config/systemSettings.json?timestamp=' + Date.now());
+      //等待2s
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      systemConfigIsLoading.value = false;
+      console.log(response.data)
+      ElMessage.success('从服务器加载后台配置成功')
+      return response.data
+    } catch (e) {
+      console.error(e);
+      ElMessage.error('从服务器加载后台配置失败')
+      systemConfigIsLoading.value = false;
+      return [];
+    }
+  } else {
+    mobileConfigIsLoading.value = true;
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/static/config/mobileSettings.json?timestamp=' + Date.now());
+      //等待2s
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      mobileConfigIsLoading.value = false;
+      ElMessage.success('从服务器加载移动端配置成功')
+      return response.data as BaseConfig[];
+    } catch (e) {
+      console.error(e);
+      ElMessage.error('从服务器加载移动端配置失败')
+      mobileConfigIsLoading.value = false;
+      return [];
+    }
   }
 }
-
-const getDefaultSystemConfig =async()=>{//获取默认参数
-  systemConfigIsLoading.value = true;
-  try{
-    const response = await axios.get('http://127.0.0.1:8000/static/settings.json?timestamp=' + Date.now());
-    systemConfig.value = response.data ;
-    //保存到本地缓存
-    localStorage.setItem('systemConfig', JSON.stringify(response.data));
-    console.log( response.data )
-    ElMessage.success('从服务器加载配置成功')
-    //等待2s
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    systemConfigIsLoading.value = false;
-  }catch (e) {
-    console.error(e);
-    ElMessage.error('从服务器加载配置失败')
-    systemConfigIsLoading.value = false;
+const resetConfig =async(type:string)=>{//重置
+  if (type=='system'){
+    systemConfig.value =  await getDefaultConfig(type)//获取默认参数
+    if (systemConfig.value){
+      localStorage.setItem('systemConfig', JSON.stringify(systemConfig.value));//保存到本地缓存
+      localStorage.setItem('systemDefaultConfig', JSON.stringify(systemConfig.value));//保存到本地的默认参数
+    }else {
+      ElMessage.warning('重置系统配置失败')
+    }
+  }else {
+    mobileConfig.value =  await getDefaultConfig(type)//获取默认参数
+    if (mobileConfig.value){
+      localStorage.setItem('mobileConfig', JSON.stringify(mobileConfig.value));//保存到本地缓存
+      localStorage.setItem('mobileDefaultConfig', JSON.stringify(mobileConfig.value));//保存到本地的默认参数
+    }else{
+      ElMessage.warning('重置移动端配置失败')
+    }
   }
 }
-const resetSystemConfig =async()=>{//重置
-  await getDefaultSystemConfig()
+const resetItemConfig = async (type:string,index:number)=>{
+  if (type=='system'){
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/static/config/systemSettings.json?timestamp=' + Date.now());
+      //等待2s
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      systemConfigIsLoading.value = false;
+      return response.data
+    } catch (e) {
+      console.error(e);
+      systemConfigIsLoading.value = false;
+    }
+  }else {
+  }
+}
+const init = async () => {
+  let systemConfigLocal = localStorage.getItem('systemConfig');
+  if (systemConfigLocal === null) {
+    ElMessage.warning('系统本地配置为空，正在从服务器加载系统默认配置')
+    await resetConfig('system')
+  }else {
+    systemConfig.value = JSON.parse(localStorage.getItem('systemConfig') || '[]');
+  }
+  let mobileConfigLocal = localStorage.getItem('mobileConfig');
+  if (mobileConfigLocal === null) {
+    ElMessage.warning('移动端本地配置为空，正在从服务器加载移动端默认配置')
+    await resetConfig('mobile')
+  }else {
+    mobileConfig.value = JSON.parse(localStorage.getItem('mobileConfig') || '[]');
+  }
+};
+const resetDefaultConfig=async (type:string)=>{
+  if (type=='system'){
+    systemDefaultConfig.value =  await getDefaultConfig(type)//获取默认参数
+    if (systemConfig.value){
+      localStorage.setItem('systemDefaultConfig', JSON.stringify( systemDefaultConfig.value));//保存到本地缓存
+    }else {
+      ElMessage.warning('获取系统默认配置失败')
+    }
+  }else{
+    mobileDefaultConfig.value =  await getDefaultConfig(type)//获取默认参数
+    if (mobileConfig.value){
+      localStorage.setItem('mobileDefaultConfig', JSON.stringify(mobileDefaultConfig.value));//保存到本地缓存
+    }else {
+      ElMessage.warning('获取移动端配置失败')
+    }
+  }
 }
 init();
-
 </script>
 <template >
   <div style="width: 100%" class="setting-content">
         <el-descriptions title="后台系统配置" v-loading="systemConfigIsLoading" border :column="2" style="width: 100%">
-        <el-descriptions-item :label="item.name" v-for="item in systemConfig">
+          <el-descriptions-item :label="item.name" v-for="item in systemConfig">
+            <template #default>
+              <el-input style="width: 50%" v-if="item.type==0" v-model="item.default" :placeholder="item.tip"></el-input>
+              <el-select style="width: 50%" v-if="item.type==1" placeholder="请选择" v-model="item.default">
+                <el-option v-for="option in item.content" :key="option" :label="option" :value="option" />
+              </el-select>
+              <el-button size="small"  style="margin-left: 5%" type="danger" @click="">
+                重置
+              </el-button>
+              <el-button size="small"  type="success" @click="">
+                保存
+              </el-button>
+              <el-button size="small"  type="primary" @click="">
+                刷新
+              </el-button>
+          </template>
+        </el-descriptions-item>
+          <el-descriptions-item label="全局配置">
           <template #default>
-            <el-input style="width: 50%" v-if="item.type==0" v-model="item.default" :placeholder="item.tip"></el-input>
-            <el-select style="width: 50%" v-if="item.type==1" placeholder="请选择" v-model="item.default">
-              <el-option v-for="option in item.content" :key="option" :label="option" :value="option" />
-            </el-select>
-            <el-button  style="margin-left: 5%" size="small" type="warning" @click="">
-              确定
-            </el-button>
             <el-button size="small"  type="danger" @click="">
               重置
             </el-button>
@@ -70,35 +151,25 @@ init();
             <el-button size="small"  type="primary" @click="">
               刷新
             </el-button>
-          </template>
-        </el-descriptions-item>
-        <el-descriptions-item label="全局配置">
-          <template #default>
-            <el-button  style="margin-left: 5%" size="small" type="warning" @click="">
-              确定
+            <el-button size="small"  type="warning" @click="resetConfig('system')">
+              从服务器更新系统配置
             </el-button>
-            <el-button size="small"  type="danger" @click="resetSystemConfig">
-              恢复默认
-            </el-button>
-            <el-button size="small"  type="success" @click="">
-              保存
-            </el-button>
-            <el-button size="small"  type="primary" @click="">
-              刷新
+            <el-button size="small"  type="warning" @click="resetDefaultConfig('system')">
+              仅更新默认系统配置
             </el-button>
           </template>
         </el-descriptions-item>
       </el-descriptions>
       </div>
   <div style="width: 100%" class="setting-content">
-    <el-descriptions title="移动端配置"  border :column="2" style="width: 100%">
-      <el-descriptions-item label="IP地址">
+    <el-descriptions title="移动端配置" v-loading="mobileConfigIsLoading" border :column="2" style="width: 100%">
+      <el-descriptions-item :label="item.name" v-for="item in mobileConfig">
         <template #default>
-          <el-input style="width: 50%" placeholder="请输入内容"></el-input>
-          <el-button  style="margin-left: 5%" size="small" type="warning" @click="">
-            确定
-          </el-button>
-          <el-button size="small"  type="danger" @click="">
+          <el-input style="width: 50%" v-if="item.type==0" v-model="item.default" :placeholder="item.tip"></el-input>
+          <el-select style="width: 50%" v-if="item.type==1" placeholder="请选择" v-model="item.default">
+            <el-option v-for="option in item.content" :key="option" :label="option" :value="option" />
+          </el-select>
+          <el-button size="small" style="margin-left: 5%"  type="danger" @click="">
             重置
           </el-button>
           <el-button size="small"  type="success" @click="">
@@ -109,13 +180,9 @@ init();
           </el-button>
         </template>
       </el-descriptions-item>
-      <el-descriptions-item label="端口">
+      <el-descriptions-item label="全局配置">
         <template #default>
-          <el-input style="width: 50%" placeholder="请输入内容"></el-input>
-          <el-button  style="margin-left: 5%" size="small" type="warning" @click="">
-            确定
-          </el-button>
-          <el-button size="small"  type="danger" @click="">
+          <el-button size="small"  type="danger" @click="resetConfig('mobile')">
             重置
           </el-button>
           <el-button size="small"  type="success" @click="">
